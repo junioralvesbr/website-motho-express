ef0fc48bf4ec9e42d797e27aff150f3e12fc6fdb
Junior Alves <jrnalves@gmail.com>
2023-05-04 00:48:31 -0300

    first commit

---
+++ js/tailwind-config.js
@@ -0,0 +1,16 @@
+export default tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        Rubik: ['Rubik', 'sans-serif'],
+        Roboto: ['Roboto', 'sans-serif']
+      },
+      colors: {
+        primary: '#44413E',
+        secondary: '#99948F',
+        third: '#871159',
+        white: '#fff'
+      },
+    }
+  }
+}
